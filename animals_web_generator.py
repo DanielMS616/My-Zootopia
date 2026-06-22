3553339a6ded92a6ad566c8de17f18fe96427c40
Daniel Glöde <dgms@MacBook-Air-von-Daniel.fritz.box>
2026-06-23 01:33:21 +0200

    Improve HTML of card items

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -4,7 +4,7 @@ import json
 def load_data(file_path):
     """Loads a JSON file."""
 
-    with open(file_path, "r") as handle:
+    with open(file_path, "r", encoding="utf-8") as handle:
         return json.load(handle)
 
 
@@ -16,29 +16,32 @@ for animal in animals_data:
     output += '<li class="cards__item">\n'
 
     if "name" in animal:
-        output += f"Name: {animal['name']}<br/>\n"
+        output += f'  <div class="card__title">{animal["name"]}</div>\n'
+
+    output += '  <p class="card__text">\n'
 
     if "characteristics" in animal:
         if "diet" in animal["characteristics"]:
-            output += f"Diet: {animal['characteristics']['diet']}<br/>\n"
+            output += f'    <strong>Diet:</strong> {animal["characteristics"]["diet"]}<br/>\n'
 
     if "locations" in animal:
         if len(animal["locations"]) > 0:
-            output += f"Location: {animal['locations'][0]}<br/>\n"
+            output += f'    <strong>Location:</strong> {animal["locations"][0]}<br/>\n'
 
     if "characteristics" in animal:
         if "type" in animal["characteristics"]:
-            output += f"Type: {animal['characteristics']['type']}<br/>\n"
+            output += f'    <strong>Type:</strong> {animal["characteristics"]["type"]}<br/>\n'
 
+    output += "  </p>\n"
     output += "</li>\n"
 
 
-with open("animals_template.html", "r") as template_file:
+with open("animals_template.html", "r", encoding="utf-8") as template_file:
     template = template_file.read()
 
 
 new_html = template.replace("__REPLACE_ANIMALS_INFO__", output)
 
 
-with open("animals.html", "w") as output_file:
+with open("animals.html", "w", encoding="utf-8") as output_file:
     output_file.write(new_html)
